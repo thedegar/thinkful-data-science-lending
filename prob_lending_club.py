9cd25c5e90bb46fecb8fcd3d066394397fe60e2c
Tyler Hedegard <tchedegard@gmail.com>
2016-06-03 14:39:38 +0000

    changed image names and added 2nd set of images

--- prob_lending_club.py
+++ prob_lending_club.py
@@ -11,16 +11,28 @@ import scipy.stats as stats
 
 loansData = pd.read_csv('lending_data.csv')
 # loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
-# Can't access github from work so had to download the file locally via other means...
+# Can't access github from work so had to download the file locally fia other means...
 
 loansData.dropna(inplace=True)
 
 loansData.boxplot(column='Amount.Funded.By.Investors')
-plt.savefig("images/boxplot.png")
+plt.savefig("images/amount_funded_boxplot.png")
 
 loansData.hist(column='Amount.Funded.By.Investors')
-plt.savefig("images/histogram.png")
+plt.savefig("images/amount_funded_histogram.png")
 
 plt.figure()
 graph = stats.probplot(loansData['Amount.Funded.By.Investors'], dist="norm", plot=plt)
-plt.savefig("images/qqplot.png")
+plt.savefig("images/amount_funded_qqplot.png")
+
+plt.figure()
+
+loansData.boxplot(column='Amount.Requested')
+plt.savefig("images/amount_requested_boxplot.png")
+
+loansData.hist(column='Amount.Requested')
+plt.savefig("images/amount_requested_histogram.png")
+
+plt.figure()
+graph = stats.probplot(loansData['Amount.Requested'], dist="norm", plot=plt)
+plt.savefig("images/amount_requested_qqplot.png")
